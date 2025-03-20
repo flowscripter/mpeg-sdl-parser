4241980d0b4e2d72fb2ead2ed222463e17c974a6
vectronic <hello.vectronic@gmail.com>
2025-03-20 09:42:08 +0000

    test: add test for for loop with expression1 empty

--- tests/parser/syntax_patterns/for_statement_pattern_test.ts
+++ tests/parser/syntax_patterns/for_statement_pattern_test.ts
@@ -438,4 +438,166 @@ describe("For Statement Pattern Tests", () => {
       ),
     );
   });
+
+  test("Test for statement pattern - expression1 is empty", () => {
+    testSyntaxPattern(
+      FOR_STATEMENT_RULE,
+      "for (;i<9;i++) {j++;}",
+      new ForStatement(
+        undefined,
+        undefined,
+        new BinaryExpression(
+          new Identifier(
+            "i",
+            new SyntaxToken(
+              TokenKind.IDENTIFIER_TOKEN,
+              { row: 0, column: 6, position: 6 },
+              "i",
+              [],
+              [],
+            ),
+          ),
+          BinaryOperatorKind.LESS_THAN,
+          new NumberLiteral(
+            NumberLiteralKind.INTEGER,
+            9,
+            [
+              new SyntaxToken(
+                TokenKind.LITERAL_INTEGER_TOKEN,
+                { row: 0, column: 8, position: 8 },
+                "9",
+                [],
+                [],
+              ),
+            ],
+          ),
+          new SyntaxToken(
+            TokenKind.OPERATOR_LESS_THAN_TOKEN,
+            { row: 0, column: 7, position: 7 },
+            "<",
+            [],
+            [],
+          ),
+        ),
+        new PostfixExpression(
+          new Identifier(
+            "i",
+            new SyntaxToken(
+              TokenKind.IDENTIFIER_TOKEN,
+              { row: 0, column: 10, position: 10 },
+              "i",
+              [],
+              [],
+            ),
+          ),
+          undefined,
+          undefined,
+          PostfixOperatorKind.POSTFIX_INCREMENT,
+          new SyntaxToken(
+            TokenKind.OPERATOR_POSTFIX_INCREMENT_TOKEN,
+            { row: 0, column: 11, position: 11 },
+            "++",
+            [],
+            [],
+          ),
+        ),
+        new CompoundStatement(
+          [
+            new ExpressionStatement(
+              new PostfixExpression(
+                new Identifier(
+                  "j",
+                  new SyntaxToken(
+                    TokenKind.IDENTIFIER_TOKEN,
+                    { row: 0, column: 16, position: 16 },
+                    "j",
+                    [],
+                    [],
+                  ),
+                ),
+                undefined,
+                undefined,
+                PostfixOperatorKind.POSTFIX_INCREMENT,
+                new SyntaxToken(
+                  TokenKind.OPERATOR_POSTFIX_INCREMENT_TOKEN,
+                  { row: 0, column: 17, position: 17 },
+                  "++",
+                  [],
+                  [],
+                ),
+              ),
+              new SyntaxToken(
+                TokenKind.PUNCTUATOR_SEMICOLON_TOKEN,
+                { row: 0, column: 19, position: 19 },
+                ";",
+                [],
+                [],
+              ),
+            ),
+          ],
+          new SyntaxToken(
+            TokenKind.PUNCTUATOR_OPEN_BRACE_TOKEN,
+            { row: 0, column: 15, position: 15 },
+            "{",
+            [],
+            [],
+          ),
+          new SyntaxToken(
+            TokenKind.PUNCTUATOR_CLOSE_BRACE_TOKEN,
+            { row: 0, column: 20, position: 20 },
+            "}",
+            [],
+            [],
+          ),
+        ),
+        new SyntaxToken(
+          TokenKind.KEYWORD_FOR_TOKEN,
+          { row: 0, column: 0, position: 0 },
+          "for",
+          [],
+          [
+            new Trivia(TokenKind.WHITESPACE_TOKEN, {
+              row: 0,
+              column: 3,
+              position: 3,
+            }, " "),
+          ],
+        ),
+        new SyntaxToken(
+          TokenKind.PUNCTUATOR_OPEN_PARENTHESIS_TOKEN,
+          { row: 0, column: 4, position: 4 },
+          "(",
+          [],
+          [],
+        ),
+        new SyntaxToken(
+          TokenKind.PUNCTUATOR_SEMICOLON_TOKEN,
+          { row: 0, column: 5, position: 5 },
+          ";",
+          [],
+          [],
+        ),
+        new SyntaxToken(
+          TokenKind.PUNCTUATOR_SEMICOLON_TOKEN,
+          { row: 0, column: 9, position: 9 },
+          ";",
+          [],
+          [],
+        ),
+        new SyntaxToken(
+          TokenKind.PUNCTUATOR_CLOSE_PARENTHESIS_TOKEN,
+          { row: 0, column: 13, position: 13 },
+          ")",
+          [],
+          [
+            new Trivia(TokenKind.WHITESPACE_TOKEN, {
+              row: 0,
+              column: 14,
+              position: 14,
+            }, " "),
+          ],
+        ),
+      ),
+    );
+  });
 });
